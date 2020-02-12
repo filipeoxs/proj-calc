25bd47a1c8bc65ea5cbc296c8d2336f3677d0b96
Filipe Castro <filipeoxs@gmail.com>
2020-02-12 11:33:40 -0300

    First

--- Calculadora_explicativa-v1.1.py
+++ Calculadora_explicativa-v1.1.py
@@ -1,73 +1,116 @@
-import math
+from math import pow, sqrt
+from time import sleep
+
 
 def msg():
+    while True:
+        inicio()
+        try:
+            home = int(input('Escolha uma operação \n[+]1\n[-]2\n[*]3\n[/]4\n[n]5\n[√]6\n  => '))  # Requisita que o usuario escolha a operação
+            if home == 1:
+                soma()
+            elif home == 2:
+                subtrair()
+                break
+            elif home == 3:
+                multiplicar()
+                break
+            elif home == 4:
+                dividir()
+                break
+            elif home == 5:
+                potencia()
+                break
+            elif home == 6:
+                raiz()
+                break
+            else:
+                print('Opcão inválida!')
+                sleep(2)
+                print('_'*20, '\n'*20)
+        except Exception as e:
+            print(f"Error! digite somente numeros.\nErro: {e}")
+            sleep(3)
+        while True:
+            finalizar = str(input('Finaliar programa? [S/N] ')).upper().strip()[0]
+            if finalizar in 'S':
+                print('finalizando...')
+                sleep(1)
+                exit()
+            elif finalizar == 'N':
+                msg()
+            else:
+                print('\n'*20)
+                print('Digite S para sair ou N paracontinuar')
+
+
+
+def inicio():
     print('''
-     =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-                  D i g i t a l
-        _____       _       __       _____
-       |     |    /   \    |  |     |     | 
-       |   __|   /  _  \   |  |     |   __| 
-       |  |     /  /_\  \  |  |     |  |    
-       |  |__  |    _    | |  |___  |  |__      
-       |     | |   / \   | |      | |     | 
-       |_____| |__|   | _| |______| |_____|   
-
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    ''')
-
-    home = int(input('Escolha uma operação \n[+]1\n[-]2\n[*]3\n[/]4\n[n]5\n[√]6\n[%]7')) # Requisita que o usuario escolha a operação
-    try:
-        num_1 = input('Insira um numero:')
-        n1 = float(num_1)
-        num_2 = input('Insira outro numero:')
-        n2 = float(num_2)
-        if home == 1:
-            soma(n1,n2)
-        elif home == 2:
-            subtrair(n1,n2)
-        elif home == 3:
-            multiplicar(n1,n2)
-        elif home == 4:
-            dividir(n1,n2)
-        elif home == 5:
-            potencia(n1,n2)
-        elif home == 6:
-            raiz()
-        else:
-            print('Escolha uma das opções acima!')
+         =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+                      D i g i t a l
+            _____       _       __       _____
+           |     |    /   \    |  |     |     | 
+           |   __|   /  _  \   |  |     |   __| 
+           |  |     /  /_\  \  |  |     |  |    
+           |  |__  |    _    | |  |___  |  |__      
+           |     | |   / \   | |      | |     | 
+           |_____| |__|   | _| |______| |_____|   
+        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        ''')
 
 
-    except Exception as e:
-        print(f"Error! digite somente numeros.\nError {e}")
+def soma():
+    n1 = float(input('Insira um numero: '))
+    n2 = float(input('Insira outro numero: '))
+    somar: int = n1 + n2
+    print('_'*20)
+    print(f'Resultado: {somar}')
+    print('_'*20)
 
 
+def subtrair():
+    n1 = float(input('Insira um numero: '))
+    n2 = float(input('Insira outro numero: '))
+    sub: int = n1 - n2
+    print('_' * 20)
+    print(f'Resultado: {sub}')
+    print('_' * 20)
 
-def soma(n1,n2):
-    somar:int = n1+n2
-    print(somar)
 
-def subtrair(n1,n2):
-    sub:int = n1-n2
-    print(sub)
+def multiplicar():
+    n1 = float(input('Insira um numero: '))
+    n2 = float(input('Insira outro numero: '))
+    mult: int = n1 * n2
+    print('_' * 20)
+    print(f'Resultado: {mult}')
+    print('_' * 20)
 
-def multiplicar(n1,n2):
-    mult:int = n1 * n2
-    print(mult)
 
-def dividir(n1,n2):
-    div:int = n1 / n2
-    resto = n1 % n2
-    print(div)
-    print(resto)
+def dividir():
+    n1 = float(input('Insira um numero: '))
+    n2 = float(input('Insira outro numero: '))
+    div: int = n1 / n2
+    print('_' * 20)
+    print(f'Resultado: {div:.2f}')
+    print('_' * 20)
 
-def potencia(n1,n2):
-    pot:int = n1 ** n2
-    print(pot)
+
+def potencia():
+    n1 = float(input('Insira um numero: '))
+    n2 = float(input('Insira o expoente: '))
+    pot: int = pow(n1, n2)
+    print('_' * 20)
+    print(f'Resultado: {pot}')
+    print('_' * 20)
+
 
 def raiz():
-    num_1 = input('Insira um numero:')
-    n1 = float(num_1)
-    rai:float = math.sqrt(n1)
-    print(rai)
+    n1 = float(input('Insira um numero: '))
+    rai: float = sqrt(n1)
+    print('_' * 20)
+    print(f'Resultado: {rai:.2f}')
+    print('_' * 20)
+
 
 msg()
